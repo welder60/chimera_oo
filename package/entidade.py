277d59eacd98190f3364aedeb222601aff4f88a5
Welder Rodrigues de Medeiros <73802580+welder60@users.noreply.github.com>
2025-10-20 18:04:42 -0300

    Add image path support to Entidade

--- package/entidade.py
+++ package/entidade.py
@@ -1,17 +1,21 @@
 class Entidade:
-	
-	def __init__(self,nome,tags):
-		self.set_nome(nome)
-		self.set_tags(tags)
-		
-	def set_nome(self,nome):
-		self._nome = nome
-		
-	def set_tags(self,tags):
-		self._tags = set(tags)
-		
-	def __str__(self):
-		return f"{self._nome}, tags: {self._tags}"
-	
-	def contem(self, outra):
-		return outra._tags.issubset(self._tags)
+
+        def __init__(self,nome,tags,caminho_imagem=None):
+                self.set_nome(nome)
+                self.set_tags(tags)
+                self.set_caminho_imagem(caminho_imagem)
+
+        def set_nome(self,nome):
+                self._nome = nome
+
+        def set_tags(self,tags):
+                self._tags = set(tags)
+
+        def set_caminho_imagem(self,caminho_imagem):
+                self._caminho_imagem = caminho_imagem
+
+        def __str__(self):
+                return f"{self._nome}, tags: {self._tags}, imagem: {self._caminho_imagem}"
+
+        def contem(self, outra):
+                return outra._tags.issubset(self._tags)
